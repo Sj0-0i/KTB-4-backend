09df322d713e1dabb691d10252aeeda2cd57adcf
Han Ju Kim <hjkim2902@gmail.com>
2025-02-26 15:07:02 +0900

    first commit

---
+++ src/main/java/com/example/ktb4/Ktb4Application.java
@@ -0,0 +1,13 @@
+package com.example.ktb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ktb4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ktb4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ktb4/Ktb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ktb4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
